3ef344aca09cdd6759843d25b49df7c7e6fc8978
Yanze LIU <yanzeliu.france@gmail.com>
2016-07-14 11:51:18 -0700

    fixed pagination error

--- blog/views.py
+++ blog/views.py
@@ -2,45 +2,30 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 2
-    template_name = 'blog/post/list.html'
+# mes problemes: 1. pagination ne marche pas (include in template)
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 2
+#     template_name = 'blog/post/list.html'
 
 
-# def post_list(request):
-#     # posts = Post.published.all()
-#     object_list = Post.published.all()
-#     """
-#     Paginator(object_list, num)
-#     """
-#     paginator = Paginator(object_list, 2)  # posts in each page
-#     page = request.GET.get('page')  # this indicate the current page number
-#     try:
-#         posts = paginator.page(page)  # obtain the object for the desired page by calling page() method
-#     except PageNotAnInteger:
-#         # if page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#         # if the page is out of range deliver the last page of results
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
-
-
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page', '')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#         return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 1)  # posts in each page
+    page = request.GET.get('page', 1)  # this indicate the current page number
+    try:
+        posts = paginator.page(page)  # obtain the object for the desired page by calling page() method
+    except PageNotAnInteger:
+        # if page is not an integer deliver the first page
+        posts = paginator.page(1)
+        # if the page is out of range deliver the last page of results
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -62,3 +47,21 @@ def post_detail(request, year, month, day, post):
     """
     post = get_object_or_404(Post, slug=post)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned.data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{}({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+        else:
+            form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
